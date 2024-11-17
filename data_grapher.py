b516f7e5e0c3d5e6a38fd14f550c94be7de45fac
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-11-16 18:19:20 -0800

    Add tkinter UI for more used friendly use

--- data_grapher.py
+++ data_grapher.py
@@ -1,41 +1,39 @@
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
+from tkinter import ttk
 import numpy as np
-import os
 import csv
+import os
 
-def plot(data_array, title):
-	eje_x = np.array([float(row[4].strip()) for row in data_array])  # Quinto valor
-	angulo_1 = np.array([float(row[0].strip()) for row in data_array])  # Primer valor
-	angulo_2 = np.array([float(row[1].strip()) for row in data_array])  # Segundo valor
+# TODO
+# 1.- Be able to change between test files with a dropdown
+# 2.- Processed data should be generated using the raw data on the readed file
+# 3.- There should be controls to modify the value of the processing parameters
 
-	# Graficar los datos
-	plt.figure(figsize=(10, 6))
-	plt.plot(eje_x, angulo_1, label='Ángulo 1', marker='o')
-	plt.plot(eje_x, angulo_2, label='Ángulo 2', marker='x')
+def plot(data_array, title, graph):
+	eje_x = np.array([float(row[4].strip()) for row in data_array])  # Get Time
+	angulo_1 = np.array([float(row[0].strip()) for row in data_array])  # Get Motor angle
+	angulo_2 = np.array([float(row[1].strip()) for row in data_array])  # Get arrow angle
 
-	plt.title(title)
-	plt.xlabel('Tiempo')
-	plt.ylabel('Ángulos')
-	plt.legend()
-	plt.grid(True)
+	# Update graph
+	graph.plot(eje_x, angulo_1, label='Ángulo 1', marker='o')
+	graph.plot(eje_x, angulo_2, label='Ángulo 2', marker='x')
+	graph.set_title(title)
+	graph.set_xlabel('Tiempo')
+	graph.set_ylabel('Ángulos')
+	graph.legend()
+	graph.grid(True)
 
 
-test_data_path = './Datos'
-test_data_files_list = [file for file in os.listdir(test_data_path) if os.path.isfile(os.path.join(test_data_path, file))]
-
-for file_name in test_data_files_list:
-	print(file_name + '\n')
-
+def update_graphs():
+	file_name = test_data_files_list[0]
 	file_path = os.path.join(test_data_path, file_name)
 	with open(file_path) as csvfile:
 		test_file_data_csvobj = csv.reader(csvfile)
 		test_file_data_list = [row for row in test_file_data_csvobj]
-		# print(test_file_data_list)
-		# for row in test_file_data_list:
-		# 	print(', '.join(row))
 
-		# Extraer los datos que necesitamos
-		
+		# Extract needed data		
 		raw_data = []
 		processed_data = []
 		test_file_processed_data_list = test_file_data_list[1:-2]
@@ -53,14 +51,44 @@ for file_name in test_data_files_list:
 				raw_data.append(test_file_processed_data_list[iter])
 			
 			iter += 1
+	
+	# Delete previous graphs
+	raw_graph.clear()
+	processed_graph.clear()
 
-		# print("Raw data")
-		# print(raw_data)
-		# print("\n\n")
-		# print("Procesed data")
-		# print(processed_data)
+	# Graph again with new data
+	plot(raw_data, 'Raw data', raw_graph)
+	plot(processed_data, 'Processed data', processed_graph)
 
-		plot(raw_data, 'Raw data')
-		plot(processed_data, 'Processed data')
-		plt.show()
-	break
+	# Redraw graphs
+	canvas.draw()
+
+
+def close_program():
+    root.quit()
+    root.destroy()
+
+
+test_data_path = './Datos'
+test_data_files_list = [file for file in os.listdir(test_data_path) if os.path.isfile(os.path.join(test_data_path, file))]
+
+root = tk.Tk()
+root.title('Visualizador de datos procesados')
+fig, (raw_graph, processed_graph) = plt.subplots(2, 1, figsize=(10, 8))
+fig.tight_layout(pad=5)
+
+# Add Matplotlib figure to the interface
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas_widget = canvas.get_tk_widget()
+canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+# Controls
+btn_update = ttk.Button(root, text='Actualizar Gráfica', command=update_graphs)
+btn_update.pack(side=tk.LEFT, padx=10, pady=10)
+
+root.protocol("WM_DELETE_WINDOW", close_program)
+
+# Executed once before the app runs to ensure the graphs have data on them from start
+update_graphs()
+
+root.mainloop()
